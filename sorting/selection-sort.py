7a15da57cf9cab5b7ab8f57a351f20d27adf91e8
earnshae <earnshae@users.noreply.github.com>
2022-05-21 21:44:52 +0000

    White space.

--- sorting/selection-sort.py
+++ sorting/selection-sort.py
@@ -14,7 +14,6 @@ def selectionsort(arr) :
         for j in range(i+1, len(arr)) :
             if arr[min_index] > arr[j] :
                 min_index = j
-
         #swap
         arr[i], arr[min_index] = arr[min_index], arr[i]
 
